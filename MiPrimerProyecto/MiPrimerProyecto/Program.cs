a98af80fb25765ae0481d895270a10282afb5e81
Jaime Arnulfo Segovia González <USSS018724@ugb.edu.sv>
2024-08-17 10:45:56 -0600

    Creación de mi primer proyecto

---
+++ MiPrimerProyecto/MiPrimerProyecto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, mundo!");
+            // Pausa el programa para que la consola no se cierree inmediatamente 
+            Console.WriteLine("Presiona cualquier tecla para salir... ");
+            Console.ReadKey();
+        }
+    }
+}
